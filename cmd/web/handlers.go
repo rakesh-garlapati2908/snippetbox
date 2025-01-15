4dc23ea66304c798609d21e4729656df92261da2
rakesh-garlapati2908 <garlapati.r@angelbroking.com>
2025-01-15 11:58:45 +0530

    4. DB driven responses

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"html/template"
+	//"html/template"
 	"net/http"
+	"snippetbox.rakesh.net/internal/models"
 	"strconv"
 )
 
@@ -15,28 +17,38 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	snippets, err := app.snippets.Latest()
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	for _, snippet := range snippets {
+		fmt.Fprintf(w, "%+v\n\n", snippet)
+	}
+
 	// Files to be used in the home page template rendering
-	files := []string{
-		"./ui/html/base.tmpl",
-		"./ui/html/partials/nav.tmpl",
-		"./ui/html/pages/home.tmpl",
-	}
-
+	//files := []string{
+	//	"./ui/html/base.tmpl",
+	//	"./ui/html/partials/nav.tmpl",
+	//	"./ui/html/pages/home.tmpl",
+	//}
+	//
 	// Parse the templates
-	ts, err := template.ParseFiles(files...)
-	if err != nil {
-		// If there's an error in parsing, return a server error
-		app.serverError(w, err)
-		return
-	}
-
-	// Execute the template with the "base" layout and no data
-	err = ts.ExecuteTemplate(w, "base", nil)
-	if err != nil {
-		// If there's an error executing the template, return a server error
-		app.serverError(w, err)
-		return
-	}
+	//ts, err := template.ParseFiles(files...)
+	//if err != nil {
+	//	// If there's an error in parsing, return a server error
+	//	app.serverError(w, err)
+	//	return
+	//}
+	//
+	//// Execute the template with the "base" layout and no data
+	//err = ts.ExecuteTemplate(w, "base", nil)
+	//if err != nil {
+	//	// If there's an error executing the template, return a server error
+	//	app.serverError(w, err)
+	//	return
+	//}
 }
 
 // Snippet view handler (to view a specific snippet)
@@ -48,9 +60,19 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		app.notFound(w)
 		return
 	}
+	//Retrieve the Snippet from the Database
+	snippet, err := app.snippets.Get(id)
+	if err != nil {
+		//Checks if the error is models.ErrNoRecord, indicating no snippet with the given ID exists. In this case, a 404 response is sent.
+		if errors.Is(err, models.ErrNoRecord) {
+			app.notFound(w)
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
 
-	// For now, just output the snippet ID (this will later be replaced with actual snippet retrieval logic)
-	fmt.Fprintf(w, "Snippet %d", id)
+	fmt.Fprintf(w, "%+v", snippet)
 }
 
 // Snippet create handler (to create a new snippet)
@@ -61,6 +83,17 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 		app.clientError(w, http.StatusMethodNotAllowed) // Respond with a 405 Method Not Allowed error
 		return
 	}
-	// For now, just output a simple message
-	w.Write([]byte("Create a new snippet!"))
+
+	//dummy data
+	title := "O snail"
+	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
+	expires := 7
+
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+	//After successfully inserting the snippet, the handler redirects the client to the view page for the new snippet.
+	http.Redirect(w, r, fmt.Sprintf("/snippet/%d", id), http.StatusSeeOther)
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,31 +1,45 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	_ "github.com/go-sql-driver/mysql"
 	"log"
 	"net/http"
 	"os"
+	"snippetbox.rakesh.net/internal/models"
 )
 
 // application struct holds loggers for error and info logging.
 type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
+	snippets *models.SnippetModel
 }
 
 func main() {
 	// Setting up the address for the HTTP server to listen on, default is ":4000"
 	addr := flag.String("addr", ":4000", "http service address")
+
+	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL datasource name")
+
 	flag.Parse()
 
 	// Create loggers for info and error messages.
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Llongfile)
 
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
+
 	// Initialize the application with the loggers
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
+		snippets: &models.SnippetModel{DB: db},
 	}
 
 	// Create a new HTTP server with specific address, error log, and handler (routes)
@@ -39,6 +53,17 @@ func main() {
 	infoLog.Printf("Starting server on %s", *addr)
 
 	// Start the HTTP server, listen for requests, and log fatal errors
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err) // If there's an error starting the server, log it and exit.
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
